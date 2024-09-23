b36d72ffc16f6fab33181768eec3e02b77c7c169
Pedro Palma <pedro.palma@uc.cl>
2024-09-22 21:52:57 -0300

    feat: Add show last episode and print q values functions

--- MonteCarlo/Main.py
+++ MonteCarlo/Main.py
@@ -3,6 +3,7 @@ from Environments.CliffEnv import CliffEnv
 
 from monte_carlo import MonteCarlo
 from policies.epsilon_greedy import EpsilonGreedy
+from episode import Episode
 
 def get_action_from_user(actions):
     print("Valid actions:")
@@ -30,29 +31,42 @@ def play(env):
     print(f"Total reward: {total_reward}")
 
 
-def play_blackjack():
+def play_blackjack(num_of_episodes, gamma, epsilon, show=False, print_values=False):
+    policy = EpsilonGreedy(epsilon)
     env = BlackjackEnv()
-    play(env)
+    monte_carlo = MonteCarlo(env, policy, gamma)
+    q_values = monte_carlo.run(num_of_episodes)
+    if show:
+        show_last_episode(env, env.action_space, q_values, policy)
+    if print_values:
+        print_q_values(q_values)
 
 
-def play_cliff():
-    cliff_width = 6
-    env = CliffEnv(cliff_width)
-    play(env)
-
-
-if __name__ == '__main__':
-    # play_blackjack()
-
-    num_of_episodes = 50000
-    gamma = 0.99
-    epsilon = 0.1
-    cliff_width = 6
+def play_cliff(num_of_episodes, gamma, epsilon, cliff_width, show=False, print_values=False):
     policy = EpsilonGreedy(epsilon)
     env = CliffEnv(cliff_width)
     monte_carlo = MonteCarlo(env, policy, gamma)
     q_values = monte_carlo.run(num_of_episodes)
+    if show:
+        show_last_episode(env, env.action_space, q_values, policy)
+    if print_values:
+        print_q_values(q_values)
 
+def show_last_episode(env, actions, q_values, policy):
+    actions = env.action_space
+    last_episode = Episode(env, policy, show=True)
+    trace = last_episode.run(actions, q_values)
+
+def print_q_values(q_values):
     print("Q-values:")
     for key, value in q_values.items():
         print(f"{key}: {value}")
+
+if __name__ == '__main__':
+
+    num_of_episodes = 500000
+    gamma = 0.99
+    epsilon = 0.1
+    cliff_width = 6
+    play_cliff(num_of_episodes, gamma, epsilon, cliff_width, show=True)
+    # play_blackjack(num_of_episodes, gamma, epsilon, show=True)
--- MonteCarlo/episode.py
+++ MonteCarlo/episode.py
@@ -1,14 +1,17 @@
 class Episode:
 
-    def __init__(self, env, policy):
+    def __init__(self, env, policy, show=False):
         self.env = env
         self.policy = policy
+        self.show = show
 
     def run(self, actions, q_values):
         trace = []
         state = self.env.reset()
         done = False
         while not done:
+            if self.show:
+                self.env.show()
             action = self.policy.get_action(state=state, q_values=q_values, actions=actions)
             next_state, reward, done = self.env.step(action)
             trace.append((state, action, reward))

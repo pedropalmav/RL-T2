ed378ce3e8602e72fd9dc077be0099492e4c6d32
Pedro Palma <pedro.palma@uc.cl>
2024-09-23 10:27:58 -0300

    feat: Add Iterative Policy Evaluation algoritm

---
+++ MDPs/iterative_policy_evaluation.py
@@ -0,0 +1,32 @@
+class IterativePolicyEvaluation:
+    def __init__(self, env, gamma=1.0, theta=0.00001):
+        self.env = env
+        self.gamma = gamma
+        self.theta = theta
+        self.V = {state: 0 for state in self.env.states}
+        self.delta = 0
+        
+    def evaluate(self, policy):
+        while True:
+            self.delta = 0
+            for state in self.env.states:
+                self.__update_v_value(state, policy)
+            if self.delta < self.theta:
+                break
+    
+    def __update_v_value(self, state, policy):
+        v = self.V[state] 
+        new_v = 0
+
+        # TODO: Refactor this to use nested sums or create a new function
+        for action in self.env.get_available_actions(state):
+            policy_prob = policy.get_prob(state, action)
+            inner_sum = 0
+            for prob, next_state, reward in self.env.get_transitions(state, a):
+                inner_sum += prob * (reward + self.gamma * self.V[next_state])
+            new_v += policy_prob * inner_sum
+        self.V[state] = new_v
+        self.delta = max(self.delta, abs(v - self.V[state]))
+
+    def get_state_value(self):
+        return self.V

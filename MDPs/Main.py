7162645a65c35d6217ffc62b4ed57f894c4e14a3
Pedro Palma <pedro.palma@uc.cl>
2024-09-25 19:57:57 -0300

    chore: Test refactor

--- MDPs/Main.py
+++ MDPs/Main.py
@@ -2,6 +2,7 @@ from iterative_policy_evaluation import IterativePolicyEvaluation
 from policies.uniform_random_policy import UniformRandomPolicy
 from policies.greedy_policy import GreedyPolicy
 from experiment import Experiment
+from Problems.GamblerProblem import GamblerProblem
 
 def evaluate_policy_on_problem(policy, problem, gamma=1, theta=0.0000000001):
     evaluator = IterativePolicyEvaluation(problem, gamma=gamma, theta=theta)
@@ -19,21 +20,10 @@ if __name__ == '__main__':
     # exp = Experiment()
     # exp.run()
 
-    # problem = CookieProblem(3)
-    # problem = GamblerProblem(0.55)
-    #sizes = [3, 4, 5, 6, 7, 8, 9, 10]
-    #ps = [0.25, 0.4, 0.55]
-    #for size in ps:
-    #    problem = GamblerProblem(size)
-    #    greedy_v_values = evaluate_greedy_policy_on_problem(problem, gamma=1, theta=0.0000000001)
-    #    print("V(initial_state) = ",greedy_v_values[problem.get_initial_state()])
-    #    print("Size = ", size)
-    #problem = GridProblem(4)
-    #policy = UniformRandomPolicy(problem)
-    # greedy_v_values = evaluate_greedy_policy_on_problem(problem, gamma=1, theta=0.0000000001)
-    # print("V(initial_state) = ",greedy_v_values[problem.get_initial_state()])
-    # v_values = evaluate_policy_on_problem(policy, problem, gamma=1.0)
-    # v_values = evaluate_greedy_policy_on_problem(problem, gamma=1, theta=0.0000000001)
-    # initial_state = problem.get_initial_state()
-    # print(v_values[initial_state])
-    #estimate_policy_for_problem(problem, gamma=0.99)
+    problem = GamblerProblem(0.55)
+    ps = [0.25, 0.4, 0.55]
+    for size in ps:
+       problem = GamblerProblem(size)
+       greedy_v_values = evaluate_greedy_policy_on_problem(problem, gamma=1, theta=0.0000000001)
+       print("V(initial_state) = ",greedy_v_values[problem.get_initial_state()])
+       print("Size = ", size)
